6f6e6a60cd815491fd90cada8d4dba6e25daded6
Dmitry Kaukov <dkaukov@opentable.com>
2022-02-09 21:21:58 +1100

    Update alsa.c

--- deps/mpg123/src/output/alsa.c
+++ deps/mpg123/src/output/alsa.c
@@ -132,11 +132,15 @@ static int initialize_device(audio_output_t *ao)
 		if(!AOQUIET) error("initialize_device(): cannot set stop threshold");
 		return -1;
 	}
-	if (snd_pcm_sw_params_set_silence_threshold(pcm, sw, period_size) < 0) {
+	
+	snd_pcm_uframes_t boundary;
+	snd_pcm_sw_params_get_boundary( sw, &boundary );
+	
+	if (snd_pcm_sw_params_set_silence_threshold(pcm, sw, 0) < 0) {
 		if(!AOQUIET) error("initialize_device(): cannot set silence threshold");
 		return -1;
 	}
-	if (snd_pcm_sw_params_set_silence_size(pcm, sw, buffer_size) < 0) {
+	if (snd_pcm_sw_params_set_silence_size(pcm, sw, boundary) < 0) {
 		if(!AOQUIET) error("initialize_device(): cannot set silence threshold");
 		return -1;
 	}
